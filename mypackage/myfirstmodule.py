b5737af09d33fc9dbac36e2056239ce5b2665232
Nicolò Scudeller <nico.scudeller@gmail.com>
2019-11-13 19:41:33 +0100

    first module, not finished

--- mypackage/myfirstmodule.py
+++ mypackage/myfirstmodule.py
@@ -1,1 +1,6 @@
 
+""def myfirstfunction(message=''):
+    """Prints a standard sentence dealing with the first module.
+    message -- a message to be printed
+    """
+    print("This is my first function, your message was: {}".format(message))""
