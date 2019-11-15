27313b9edaa88aa9677516db4efb24c7fbc044c0
Nicolò Scudeller <nico.scudeller@gmail.com>
2019-11-15 16:51:41 +0100

    first modification, add the message

--- mypackage/myfirstmodule.py
+++ mypackage/myfirstmodule.py
@@ -1,6 +1,4 @@
 
-""def myfirstfunction(message=''):
-    """Prints a standard sentence dealing with the first module.
-    message -- a message to be printed
-    """
-    print("This is my first function, your message was: {}".format(message))""
+def myfirstfunction(message=''):
+    
+    print("This is my first function, your message was: {}".format(message))
