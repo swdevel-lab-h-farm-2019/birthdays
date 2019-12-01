6b2fa9fb723acde375489427d32d642a6ed9ae44
Gap-98 <marco.bernardi@student.h-farm.com>
2019-12-01 18:35:16 +0100

    Firts try with positional argument

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("name_surname", help="display the birthdays of Albert Einstein, Benjamin Franklin, Ada Lovelace, Donald Trump, Rowan Atkinson")
+parser.add_argument("-v", "--verbosity", type=int, help="increase output verbosity")
 args = parser.parse_args()
-print('the choosen person is ' + args.name_surname)
+na_su = args.name_surname
+print('The choosen person is ' + na_su)
+if args.verbosity == 2:
+    print("The person choosen was born on {}".format(na_su, birthdays.return_birthday(na_su) ))
+elif args.verbosity == 1:
+    print("Oh! You've selected who was born on {}".format(na_su, birthdays.return_birthday(na_su)))
+else:
+    print(birthdays.return_birthday(na_su))
 
-birthdays.return_birthday(args.name_surname)
+birthdays.return_birthday(na_su)
 
