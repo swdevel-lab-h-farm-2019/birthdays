87d43aebcb43a479ec9fc30f06f55dbce7acc606
Nicolò Scudeller <nico.scudeller@gmail.com>
2019-12-02 10:17:50 +0100

    added if statement

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python3
-
 import birthdays
 import argparse
 
@@ -9,11 +7,13 @@ parser.add_argument("-v", "--verbosity", choices=[0, 1, 2], type=int, help="incr
 args = parser.parse_args()
 na_su = args.name_surname
 
-if args.verbosity == 2:
-    print("{} was born on {}".format(na_su, birthdays.return_birthday(na_su) ))
-elif args.verbosity == 1:
-    print("Oh! You've selected {}, who was born on {}".format(na_su, birthdays.return_birthday(na_su)))
+birthday = get_birthday()
+if birthday:
+    if args.verbosity == 2:
+        print("{} was born on {}".format(na_su, birthdays.get_birthday(na_su)))
+    elif args.verbosity == 1:
+        print("Oh! You've selected {}, who was born on {}".format(na_su, birthdays.get_birthday(na_su)))
+    else:
+        print(birthdays.get_birthday(na_su))
 else:
-    print(birthdays.return_birthday(na_su))
-
-birthdays.return_birthday(na_su)
+    print("Sorry, there is no birthday for {}".format(name))
