cafc0f10aedb3094c017d4449acc735e42e94cd7
garbina98 <giadaandrea.garbin@student.h-farm.com>
2019-12-04 09:38:16 -0500

    commento del codice

--- birthdays.py
+++ birthdays.py
@@ -1,3 +1,9 @@
+'''we have a file birthdays where we can clearly see the list of (in this case) 5 people: Albert Einstein,Benjamin Franklin,Ada Lovelace,Donald Trump and Rowan Atkinson. In relation with their date of birth. There are two functions: print_birthdays and return_birthdays. 
+
+The first function print a personalized message( in this case "Welcome to the birthday..."),then there is a 'for' cycle that print every name in birthdays.
+
+The second function use a 'if/else' cycle where, if the name that I insert is in birthdays, it will print the name followed by the date of birth, otherwise there is the command "else", that it will print 'Sadly, we don't have {}\'s birthday.' '''
+
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
