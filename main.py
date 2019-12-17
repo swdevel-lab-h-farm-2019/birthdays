8ffd48eb51bb78cb6fb159364077b20231e8eab9
Pietro Noceti <Pietro@MacBook-Air-di-Pietro-2.local>
2019-12-17 23:25:58 +0100

    last push to master

--- main.py
+++ main.py
@@ -1,67 +1,101 @@
-"""This is the main code for the project Birthdays. The code is based on an SQLite databse to check for user authentication and a python function with the arparse module to return the desired information"""
+"""This is the main code for the project Birthdays. The code is based on an
+SQLite databse to check for user authentication and a python function with 
+the arparse module to return the desired information"""
 
 import sqlite3
+
 import argparse
+
 import hashlib
+
 import csv
 
+
 conn = sqlite3.connect('script/pwd.db')
+
 cursor = conn.cursor()
 
-"""The arparse module is collecting user input into arguments"""
 def parse_args():
+    """collecting user input into arguments with argparse module"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', help="the username password", required=True)
-    parser.add_argument('-c', help="check for a usernamename and password" "(requires -p)", required=False)
-    parser.add_argument("-n", help = "Insert name in quotation marks to get birthday", required=False)
-    parser.add_argument("-v", help = "increase verbosity", default= False, action = "store_true", required=False)
+    parser.add_argument('-p', help='the username password',
+                        required=True)
+
+    parser.add_argument('-c', help='check for a usernamename and password'
+                        '(requires -p)',
+                        required=False)
+
+    parser.add_argument('-n', help='''Insert name in quotation marks to get
+                        birthday''',
+                        required=False)
+
+    parser.add_argument('-v', help='increase verbosity', default=False,
+                        action='store_true',
+                        required=False)
+
     return parser.parse_args()
 
-"""The arparse module is collecting user input into arguments"""
-def save_new_username(username, password):
-    global conn
-    global cursor
-    digest = hashlib.sha256(password.encode('utf-8')).hexdigest()
-    cursor.execute("INSERT OR REPLACE INTO user VALUES (?,?)",
-                   (username, digest))
-    print ("")
-    print ("Thanks for registering: you can now access the database with your credentials, use -c *username* and -p *password* to login, plus -n *name* to know name's birthday")
-    print ("")
-    conn.commit()
 
 args = parse_args()
-filename= "Data_file.csv"
+
+filename = 'Data_file.csv'
+
 
 def check_for_username(username, password):
+    """authenticating the user and returning correct birthday"""
+
     global conn
+
     global cursor
+
     digest = hashlib.sha256(password.encode('utf-8')).hexdigest()
-    rows = cursor.execute("SELECT * FROM user WHERE username=? and password=?",
-                          (username, digest))
+
+    rows = cursor.execute('''SELECT * FROM user WHERE username=? and
+                          password=?''', (username, digest))
+
     results = rows.fetchall()
-    
+
     if str(username) in str(results):
+
         if results:
+
             conn.commit()
+
             with open(filename) as f:
+
                 reader = csv.DictReader(f)
+
                 for row in reader:
-                    if row["name"] == args.n:
-                        print ("Welcome " +args.c+ ", the birthday of " + args.n+ " is: " +row['date_birth'])
+
+                    if row['name'] == args.n:
+
+                        print ('Welcome ' + args.c + ', the birthday of '
+                               + args.n
+                               + ' is: ' + row['date_birth'])
+
         else:
-            print ("Name is not present, or password is invalid")
+            print('Name is not present, or password is invalid')
 
     else:
-        print ("Unrecognised User, please try again")
+        print('Unrecognised User, please try again')
 
 
 args = parse_args()
+
 if args.c and args.p:
+
     check_for_username(args.c, args.p)
+
 if args.v is True:
-    print ("""This program returns the birthday of a famous person specified by the user.
-    Note that the program will only return the birthday of people present in the 'Data_file.csv' database.
-    To use first run 'script/dbmanager.py', then run main.py using python. Arguments are: -a: add username;
-    -p: add password or check password (required); -c: check username; -v: turn on verbosity;
-    n- in quotation marks, add name of person to research.""")
+
+    print ('''This program returns the birthday of a famous person
+           specified by the user. Note that the program will
+           only return the birthday of people present in the
+           'Data_file.csv'database. To use first run
+           'script/dbmanager.py', then run main.py using python.
+           Arguments are: -a: add username; -p: add password or check
+           password (required); -c: check username; -v: turn on
+           verbosity; n- in quotation marks, add name of
+           person to research.''')
+
 conn.close()
--- script/dbmanager.py
+++ script/dbmanager.py
@@ -1,11 +1,18 @@
+"""This file creates and populates the password database for the project"""
+
 import sqlite3
+
 import argparse
+
 import hashlib
 
 conn = None
+
 cursor = None
 
+
 def open_and_create():
+    """checking for database and creating it if negative"""
     global conn
     global cursor
     conn = sqlite3.connect('script/example-pwd.db')
@@ -19,21 +26,28 @@ def open_and_create():
                      (username TEXT, password TEXT,
                       PRIMARY KEY (username))''')
 
+
 def parse_args():
+    """storing arguments with argparse module"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', help="add a usernamename (requires -p)", required=False)
+    parser.add_argument('-a', help="add a usernamename (requires -p)",
+                        required=False)
     parser.add_argument('-p', help="the username password", required=True)
     return parser.parse_args()
 
 args = parse_args()
 
+
 def save_new_username(username, password):
+    """saving provided authenticators for a new user"""
     global conn
     global cursor
     digest = hashlib.sha256(password.encode('utf-8')).hexdigest()
     cursor.execute("INSERT OR REPLACE INTO user VALUES (?,?)",
                    (username, digest))
-    print ("Thanks for registering: you can now access the database with your credentials, use -c *username* and -p *password* to login, plus -n *name* to know name's birthday")
+    print ("""Thanks for registering: you can now access the
+    database with your credentials, use -c *username* and -p
+    *password* to login, plus -n *name* to know name's birthday""")
     conn.commit()
 
 
