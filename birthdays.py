41b767e0e153ba5c615254849d5de5e492e596f8
Pi221 <pietro.pellegrini2@gmail.com>
2023-11-15 14:54:29 +0100

    prova

--- birthdays.py
+++ birthdays.py
@@ -1,3 +1,7 @@
+'''
+ Comment the module goal here.
+'''
+
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
@@ -6,11 +10,13 @@ birthdays = {
     'Rowan Atkinson': '01/6/1955'}
 
 def print_birthdays():
+    '''Comment the function.'''
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
     for name in birthdays:
         print(name)
 
 def return_birthday(name):
+    '''Comment the function.'''
     if name in birthdays:
         print('{}\'s birthday is {}.'.format(name, birthdays[name]))
     else:
